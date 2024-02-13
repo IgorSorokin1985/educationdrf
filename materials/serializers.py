df42c45f1eeb38292e1b7f7eff5e299b8c30404e
sorokin <isorokin1985@gmail.com>
2024-02-14 01:54:24 +0300

    add filter and ordering for payments

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,15 +2,25 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ["id", "name", "description", "link"]
+
+
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    lessons = LessonsSerializer(source="lesson_set", many=True, read_only=True)
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ["id", "name", "description", "count_lessons", "lessons"]
 
     def get_count_lessons(self, instance):
         return len(Lesson.objects.all().filter(course=instance.pk))
+
+    #def get_lessons(self, instance):
+    #    return Lesson.objects.all().filter(course=instance.pk)
 
 
 class LessonSerializer(serializers.ModelSerializer):
