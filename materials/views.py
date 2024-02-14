fc63572700f41c1499497f405b4b0658b03f7938
sorokin <isorokin1985@gmail.com>
2024-02-15 01:18:34 +0300

    add CRUD fpr users

--- materials/views.py
+++ materials/views.py
@@ -3,33 +3,40 @@ from materials.serializers import CourseSerializer, LessonSerializer
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
 from materials.models import Course, Lesson
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class CourseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
