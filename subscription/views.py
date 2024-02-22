893643b3b2b8fc981081669e393bebeeab89e4a6
sorokin <isorokin1985@gmail.com>
2024-02-22 08:38:51 +0300

    add paginators

--- subscription/views.py
+++ subscription/views.py
@@ -11,17 +11,10 @@ from django.shortcuts import get_object_or_404
 class SubscriptionAPIView(APIView):
 
     def post(self, *args, **kwargs):
-<<<<<<< HEAD
-        user_id = self.request.user.pk
-        course_id = self.request.data["course"]
-        course_item = get_object_or_404(Course, pk=course_id)
-        subs_item = Subscription.objects.filter(user=user_id).filter(course=course_id).all()
-=======
         user = self.request.user
         course_id = self.request.data["course"]
         course_item = get_object_or_404(Course, pk=course_id)
         subs_item = Subscription.objects.filter(user=user).filter(course=course_id).all()
->>>>>>> origin/develop
 
         if len(subs_item) > 0:
             subscription_id = subs_item[0].pk
@@ -30,17 +23,9 @@ class SubscriptionAPIView(APIView):
             message = 'Subscription deleted'
         else:
             new_subscription = {
-<<<<<<< HEAD
-                "user_id": user_id,
-                "course_id": course_id
-            }
-            Subscription.objects.create(**new_subscription)
-            message = f'Subscription created {new_subscription}'
-=======
                 "user": user,
                 "course_id": course_id
             }
             Subscription.objects.create(**new_subscription)
             message = 'Subscription created'
->>>>>>> origin/develop
         return Response({"message": message})
